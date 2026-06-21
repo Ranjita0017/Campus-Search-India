d82f9b58a003857262e9e9d2bd681099bf162e4d
Ranjita Das <ranjitadas1001@gmail.com>
2026-06-21 12:09:47 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Campus Search-India
+A simple web application that fetches and displays college information from the Hipolabs Universities API based on the Indian state entered by the user.
---
+++ app.js
@@ -0,0 +1,34 @@
+let url = "http://universities.hipolabs.com/search?name=India";
+let ul = document.querySelector("ul");
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", async ()=>{
+    let inp = document.querySelector("input");
+    let state = inp.value;
+    inp.value = "";
+
+    let colArr = await getColleges();
+    showArr(colArr,state);
+})
+
+function showArr(colArr,state) {
+    let li = document.querySelector("#list");
+    li.innerText="";
+    for(col of colArr){
+        if(col['state-province'] == state){
+            let li = document.createElement("li");
+            li.innerText = col.name;
+            ul.appendChild(li);
+        }
+    }
+}
+
+
+async function getColleges() {
+    try{
+        let res = await axios.get(url);
+        return res.data;
+    }catch(e){
+        console.log("error: ",e);
+    }
+}
